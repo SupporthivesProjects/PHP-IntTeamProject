01e77bb408bdce50026cb13d172ffebb26613391
Swarali <Swarali@supporthives.com>
2025-04-17 13:16:03 +0530

    Login File Created

---
+++ login.php
@@ -0,0 +1,22 @@
+<?php
+echo"Swarali";
+?>
+
+<!DOCTYPE html>
+<html>
+<body>
+
+<h2>Swarali</h2>
+
+<form>
+  <label for="fname">First name:</label><br>
+  <input type="text" id="fname" name="fname" value="John"><br>
+  <label for="lname">Last name:</label><br>
+  <input type="text" id="lname" name="lname" value="Doe">
+</form>
+
+<p>my name is Swarali.</p>
+
+
+</body>
+</html>
